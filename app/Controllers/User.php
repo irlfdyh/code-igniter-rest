035b5038a085a35cd1205818eca930466dcca9fd
Irshal Firdeansyah <irshalfir@gmail.com>
2023-12-28 13:34:21 +0700

    implement user controller

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -3,11 +3,17 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
+use CodeIgniter\API\ResponseTrait;
 
 class User extends BaseController
 {
+
+    use ResponseTrait;
+
     public function index()
     {
-        //
+        $users = new UserModel();
+        return $this->respond(['users' => $users->findAll()], 200);
     }
 }
