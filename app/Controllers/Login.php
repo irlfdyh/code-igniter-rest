729aa9aa2c125282ab6da26c72c649ddcf34c6c8
Irshal Firdeansyah <irshalfir@gmail.com>
2023-12-28 13:23:13 +0700

    implement login controller

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -3,11 +3,59 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
+use CodeIgniter\API\ResponseTrait;
+use Firebase\JWT\JWT;
 
 class Login extends BaseController
 {
+
+    use ResponseTrait;
+
     public function index()
     {
-        //
+        $userModel = new UserModel();
+
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('password');
+
+        $user = $userModel->where('email', $email)->first();
+
+        if (is_null($user)) {
+            return $this->respond([
+                'error' => 'Invalid username or password'
+            ], 401);
+        }
+
+        $pwdVerify = password_verify($password, $user['password']);
+
+        if (!$pwdVerify) {
+            return $this->respond([
+                'error' => 'Invalid username or password'
+            ], 401);
+        }
+
+        $key = getenv('JWT_SECRET');
+        $iat = time();
+        $exp = $iat + 3600;
+
+        $payload = array(
+            "iss" => "Issuer of the JWT",
+            "aud" => "Audience that the JWT",
+            "sub" => "Subject of the JWT",
+            "iat" => $iat, //Time the JWT issued at
+            "exp" => $exp, // Expiration time of token
+            "email" => $user['email'],
+        );
+
+        $token = JWT::encode($payload, $key, 'HS256');
+
+        $response = [
+            'message' => 'Login Successful',
+            'token' => $token
+        ];
+
+        return $this->respond($response, 200);
     }
+
 }
